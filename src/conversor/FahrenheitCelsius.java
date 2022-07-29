c83acc403b96944a9eeceb1edd9aa9bc98fc5ee5
Miguel Arcanjo <945507756m@gmail.com>
2022-07-29 16:21:23 -0300

    first commit

---
+++ src/conversor/FahrenheitCelsius.java
@@ -0,0 +1,30 @@
+package conversor;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class FahrenheitCelsius {
+
+	public static void main(String[] args) {
+		// variaveis
+		double c, f;
+
+		// objetos
+		Scanner teclado = new Scanner(System.in);
+		DecimalFormat formatador = new DecimalFormat("#0.0");
+
+		// entrada
+		System.out.println("Conversão de temperatura");
+		System.out.print("Digite a temperatura em fahrenheit: ");
+		f = teclado.nextDouble();
+
+		// processamento
+		c = (5 * (f - 32)) / 9;
+
+		// saida
+		System.out.println("Temperatura em Celsius: " + formatador.format(c) + "°C");
+		teclado.close();
+
+	}
+
+}
